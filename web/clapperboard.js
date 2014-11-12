50527a6cadff5ee90300510af1e3a7ac211e9f26
Andriy Yurchuk <ayurchuk@minuteware.net>
2014-11-12 21:00:38 +0200

    Fix endpoint in web UI

--- web/clapperboard.js
+++ web/clapperboard.js
@@ -1,5 +1,5 @@
 angular.module('Clapperboard', [])
-    .constant('ENDPOINT_URI', 'http://localhost:5000/')
+    .constant('ENDPOINT_URI', 'http://minuteware.net/clapperboard/')
     .controller('MainCtrl', function (ItemsModel) {
         var main = this;
 
