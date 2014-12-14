4515f678127ac94357cbd47b6fd704af43278147
Andriy Yurchuk <ayurchuk@minuteware.net>
2014-12-14 16:12:05 +0200

    Install logrotate config

--- deploy/ansible/do_deploy.sh
+++ deploy/ansible/do_deploy.sh
@@ -2,8 +2,8 @@
 
 if [[ -f $HOME/.cb_ansible.cfg ]]; then
     echo "Using config ~/.cb_ansible.cfg"
-    ansible-playbook -vvvvvv -i production site.yml -e "@$HOME/.cb_ansible.cfg"
+    ansible-playbook -i production site.yml -e "@$HOME/.cb_ansible.cfg" --ask-sudo-pass
 else
     echo "Using default config"
-    ansible-playbook -i production site.yml
+    ansible-playbook -i production site.yml --ask-sudo-pass
 fi
